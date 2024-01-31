bb31358990fd3cae8aabd95c02b7aa42bab741cc
naturemistake666 <kindialov@yandex.ru>
2024-01-31 17:52:48 +0300

    Create Entities ROle and User

--- src/main/java/com/aston/WeatherAPI/repository/WeatherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aston.WeatherAPI.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WeatherRepository extends JpaRepository<Weather, Long>{
-
-}
